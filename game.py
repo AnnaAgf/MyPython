bc2fc9393e1776dcf436ad8f260d5838ffb811ec
Anna.Agafonova <annvuela@gmail.com>
2018-11-29 15:40:15 +0300

    Merge branch 'three'
    # Conflicts:
    #	tale

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import random
 
+MAX_ERRORS = 8
+
 world_list = ('way', 'oil', 'cosmic', 'plan', 'library', 'washer', 'olimpia')
 
 
@@ -14,5 +16,35 @@ print(secret_word)
 users_word = ['*'] * len(secret_word)
 print_users_word(users_word)
 
-users_word[1] = 's'
-print_users_word(users_word)
+#users_word[1] = 's'
+#print_users_word(users_word)
+
+
+errors_counter = 0
+while True:
+    letter = raw_input("vvedi bukvu ")
+
+    if len(letter) != 1 or not letter.isalpha():
+        continue
+
+    if letter in secret_word:
+        idx = 0
+        for char in secret_word:
+            if char == letter:
+                users_word[idx] = letter
+            idx += 1
+        if '*' not in users_word:
+            print('win ;)')
+            break
+
+    else:
+        errors_counter += 1
+        print('mistakes are ', errors_counter)
+        if errors_counter == MAX_ERRORS:
+            print('loser')
+            break
+    print_users_word(users_word)
+print('Bye!')
+
+    #print(letter)
+
